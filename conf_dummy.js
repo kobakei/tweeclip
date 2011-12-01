d58c0d40ff3476f8d50785680218ba5a60221ec4
keisuke <keisuke@keisuke-mint64.(none)>
2011-12-01 17:47:20 +0900

    conf_dummy comment

--- conf_dummy.js
+++ conf_dummy.js
@@ -1,3 +1,7 @@
+/**
+ * - Please get conusmer key/secret form dev.twitter.com
+ * - Please rename this file to "conf.js"
+ */
 exports.twit = {};
 exports.twit.consumerKey = "YOUR CONSUMER KEY"
 exports.twit.consumerSecret = "YOUR CONSUMER SECRET"
